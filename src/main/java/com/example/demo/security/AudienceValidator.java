2facf5debe70373d7985f02c906a2a12a4cfb95e
Fulano2141 <macuto2142dt@gmail.com>
2021-05-24 22:00:35 -0400

    Auth0 config

--- src/main/java/com/example/demo/security/AudienceValidator.java
+++ src/main/java/com/example/demo/security/AudienceValidator.java
@@ -1,25 +1,31 @@
 package com.example.demo.security;
 
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.core.OAuth2Error;
 import org.springframework.security.oauth2.core.OAuth2TokenValidator;
 import org.springframework.security.oauth2.core.OAuth2TokenValidatorResult;
 import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.stereotype.Component;
 
 /**
  * Validates that the JWT token contains the intended audience in its claims.
  */
-@RequiredArgsConstructor(staticName = "of")
-public class AudienceValidator implements OAuth2TokenValidator<Jwt> {
-    private final String audience;
+public class AudienceValidator {
 
-    public OAuth2TokenValidatorResult validate(Jwt jwt) {
-        OAuth2Error error = new OAuth2Error("invalid_token", "The required audience is missing", null);
-
-        if (jwt.getAudience().contains(audience)) {
-            return OAuth2TokenValidatorResult.success();
-        }
-
-        return OAuth2TokenValidatorResult.failure(error);
-    }
+//    private final String audience;
+//
+//    OAuth2Error error = new OAuth2Error("invalid_token", "The required audience is missing", null);
+//
+//    public AudienceValidator(String audience) {
+//        this.audience = audience;
+//    }
+//
+//    public OAuth2TokenValidatorResult validate(Jwt jwt) {
+//        if (jwt.getAudience().contains(audience)) {
+//            return OAuth2TokenValidatorResult.success();
+//        }
+//
+//        return OAuth2TokenValidatorResult.failure(error);
+//    }
 }
--- src/main/java/com/example/demo/util/SecurityConfig.java
+++ src/main/java/com/example/demo/util/SecurityConfig.java
@@ -1,56 +1,53 @@
 package com.example.demo.util;
 
-import com.example.demo.security.AudienceValidator;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
-import org.springframework.security.oauth2.core.OAuth2TokenValidator;
-import org.springframework.security.oauth2.jwt.*;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-public class SecurityConfig {
-    @Bean
-    SecurityFilterChain springWebFilterChain(HttpSecurity http) throws Exception {
-        return http
-                .httpBasic(AbstractHttpConfigurer::disable)
-                .csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement(c -> c.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeRequests(c -> c
-                        .antMatchers("/", "/info").permitAll()
-                        .antMatchers(HttpMethod.GET, "/posts/**").permitAll()//.hasAuthority("SCOPE_read:posts")
-                        .antMatchers(HttpMethod.POST, "/posts/**").hasAuthority("SCOPE_write:posts")// access("#oauth2.hasScope('write:posts')")
-                        .antMatchers(HttpMethod.PUT, "/posts/**").hasAuthority("SCOPE_write:posts")
-                        .antMatchers(HttpMethod.DELETE, "/posts/**").hasAuthority("SCOPE_delete:posts")
-                        .anyRequest().authenticated()
-                )
-                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
-                .build();
-    }
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+//    private String issuer;
+//
+//    @Value("${auth0.audience}")
+//    private String audience;
 
-    @Bean
-    JwtDecoder jwtDecoder(OAuth2ResourceServerProperties properties, @Value("${auth0.audience}") String audience) {
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
         /*
-        By default, Spring Security does not validate the "aud" claim of the token, to ensure that this token is
-        indeed intended for our app. Adding our own validator is easy to do:
+        This is where we configure the security required for our endpoints and setup our app to serve as
+        an OAuth2 Resource Server, using JWT validation.
         */
-
-        String issuerUri = properties.getJwt().getIssuerUri();
-        NimbusJwtDecoder jwtDecoder = (NimbusJwtDecoder) JwtDecoders.fromOidcIssuerLocation(issuerUri);
-
-        OAuth2TokenValidator<Jwt> audienceValidator = AudienceValidator.of(audience);
-        OAuth2TokenValidator<Jwt> withIssuer = JwtValidators.createDefaultWithIssuer(issuerUri);
-        OAuth2TokenValidator<Jwt> withAudience = new DelegatingOAuth2TokenValidator<>(withIssuer, audienceValidator);
-
-        jwtDecoder.setJwtValidator(withAudience);
-
-        return jwtDecoder;
+        http.cors()
+                .and().authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/data/last/day/*").permitAll()
+                .antMatchers(HttpMethod.GET, "/data/general/*").permitAll()
+                .antMatchers(HttpMethod.GET, "/data/general/list/*").permitAll()
+                .antMatchers(HttpMethod.GET, "/view/*/list").authenticated()
+                .antMatchers(HttpMethod.POST, "/view").hasAuthority("SCOPE_create:user_dashboards")
+                .anyRequest()
+                .authenticated().and().oauth2ResourceServer()
+                .jwt();
     }
+
+//    @Bean
+//    ReactiveJwtDecoder jwtDecoder() {
+//        /*
+//        By default, Spring Security does not validate the "aud" claim of the token, to ensure that this token is
+//        indeed intended for our app. Adding our own validator is easy to do:
+//        */
+//
+//        NimbusReactiveJwtDecoder jwtDecoder = (NimbusReactiveJwtDecoder)
+//                ReactiveJwtDecoders.fromOidcIssuerLocation(issuer);
+//
+//        OAuth2TokenValidator<Jwt> audienceValidator = new AudienceValidator(audience);
+//        OAuth2TokenValidator<Jwt> withIssuer = JwtValidators.createDefaultWithIssuer(issuer);
+//        OAuth2TokenValidator<Jwt> withAudience = new DelegatingOAuth2TokenValidator<>(withIssuer, audienceValidator,
+//                new JwtTimestampValidator());
+//
+//        jwtDecoder.setJwtValidator(withAudience);
+//
+//        return jwtDecoder;
+//    }
 }
